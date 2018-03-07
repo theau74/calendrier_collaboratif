af86b9ba0fa68c4519d680a1950ad8611c3ce5cd
TheoTaiTroSek <b.favre.teylaz@gmail.com>
2018-03-07 15:04:12 +0100

    mise en place calendrier

--- view/header.php
+++ view/header.php
@@ -8,10 +8,7 @@
     <meta name="viewport" content="width=device-width, user-scalable=no">
     <link href="asset/css/reset.css" rel="stylesheet" type="text/css" />
     <link href="asset/css/main.css" rel="stylesheet" type="text/css" />
-    <link href="asset/css/calendrier.css" rel="stylesheet" type="text/css"/>
     <script type="text/javascript" src="asset/js/pre_load.js"></script>
-    <script type="text/javascript" src="asset/js/calendrier.js"></script>
-    <script type="text/javascript" src="../asset/js/script.js"></script>
     <link rel='stylesheet' href='asset/css/fullcalendar.css' />
     <script src='asset/js/jquery.min.js'></script>
     <script src='asset/js/moment.min.js'></script>
