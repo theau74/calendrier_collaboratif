780064e0f5ce275f67d5f57cf06737efa0210630
TheoTaiTroSek <b.favre.teylaz@gmail.com>
2018-03-07 14:06:41 +0100

    mise en place calendrier

--- view/header.php
+++ view/header.php
@@ -8,10 +8,13 @@
     <meta name="viewport" content="width=device-width, user-scalable=no">
     <link href="asset/css/reset.css" rel="stylesheet" type="text/css" />
     <link href="asset/css/main.css" rel="stylesheet" type="text/css" />
-    <link href="../asset/css/calendrier.css" rel="stylesheet" type="text/css"/>
     <script type="text/javascript" src="asset/js/pre_load.js"></script>
-    <script type="text/javascript" src="../asset/js/calendrier.js"></script>
     <script type="text/javascript" src="../asset/js/script.js"></script>
+    <link rel='stylesheet' href='asset/calendar/fullcalendar.css' />
+    <script src='asset/calendar/lib/jquery.min.js'></script>
+    <script src='asset/calendar/lib/moment.min.js'></script>
+    <script src='asset/calendar/fullcalendar.js'></script>
+    <script src='asset/calendar/locale/fr.js'></script>
     <title>Agenda Collaboratif - Agenda</title>
     <meta name="keywords"/>
     <meta name="description"/>
@@ -19,8 +22,8 @@
 </head>
 <body>
 
-    <div class="of-container">
+<div class="of-container">
 
-        <?php
-        display_user_session();
-        ?>
+<?php
+display_user_session();
+?>
