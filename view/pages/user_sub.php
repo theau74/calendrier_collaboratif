799339abe47028b3911c9ffe5ba26cce311b175f
TheoT <trolle.theo@gmail.com>
2018-03-11 19:13:52 +0100

    Correction Inscription

--- view/pages/user_sub.php
+++ view/pages/user_sub.php
@@ -1,60 +1,64 @@
-<div class="ac-header-home">
+<div class="ac-home-header">
 
-    <h1 class="ac-header-home-title">Agenda Collaboratif</h1>
+    <h1 class="ac-home-header-title">Agenda Collaboratif</h1>
 
 </div>
 
-<div class="ac-home ac-home-signup">
+<div class="ac-home-container ac-home-signup">
 
-    <form action="index.php?ac=signup" method="post">
+    <div class="ac-home">
 
-        <ul>
+        <form action="index.php?ac=signup" method="post">
 
-            <li class="ac-home-sign-item">
-                <input type="text" class="ac-home-sign-item-input" name="Pseudo" placeholder="Pseudo" required>
-            </li>
+            <ul>
 
-            <li class="ac-home-sign-item">
-                <input type='password' class="ac-home-sign-item-input" name="Mot de passe" placeholder="Mot de passe" required>
-            </li>
+                <li class="ac-home-sign-item">
+                    <input type="text" class="ac-home-sign-item-input" name="Pseudo" placeholder="Pseudo" required>
+                </li>
 
-            <li class="ac-home-sign-item">
-                <input class="ac-home-sign-item-input" type="text" name='email' placeholder='Adresse e-mail' required/>
-            </li>
+                <li class="ac-home-sign-item">
+                    <input type='password' class="ac-home-sign-item-input" name="Mot de passe" placeholder="Mot de passe" required>
+                </li>
 
-            <li class="ac-home-sign-item">
-                <input class="ac-home-sign-item-input" type="text" placeholder='Prenom' name='fname' required/>
-            </li>
+                <li class="ac-home-sign-item">
+                    <input class="ac-home-sign-item-input" type="text" name='email' placeholder='Adresse e-mail' required/>
+                </li>
 
-            <li class="ac-home-sign-item">
-                <input class="ac-home-sign-item-input" type="text" placeholder='Nom' name='lname' required/>
-            </li>
+                <li class="ac-home-sign-item">
+                    <input class="ac-home-sign-item-input" type="text" placeholder='Prenom' name='fname' required/>
+                </li>
 
-            <li class="ac-home-sign-item">
-                <input class="ac-home-sign-item-input" type="text" placeholder='Téléphone' name='tel' required/>
-            </li>
+                <li class="ac-home-sign-item">
+                    <input class="ac-home-sign-item-input" type="text" placeholder='Nom' name='lname' required/>
+                </li>
 
-            <div class="ac-home-sign-allBoutton">
-                <ul>
+                <li class="ac-home-sign-item">
+                    <input class="ac-home-sign-item-input" type="text" placeholder='Téléphone' name='tel' required/>
+                </li>
 
-                    <li class="ac-home-sign-item-boutton-left">
-                        <button type="submit" class="ac-home-sign-item-boutton-log" name="subscribe">
-                            Validé
-                        </button>
-                    </li>
+                <div class="ac-home-sign-allBoutton">
+                    <ul>
 
-                    <li class="ac-home-sign-item-boutton-right">
-                        <a href="index.php" class="ac-home-sign-item-boutton-sign">
-                            Connexion
-                        </a>
-                    </li>
+                        <li class="ac-home-sign-item-boutton-left">
+                            <button type="submit" class="ac-home-sign-item-boutton-log" name="subscribe">
+                                Validé
+                            </button>
+                        </li>
 
-                </ul>
+                        <li class="ac-home-sign-item-boutton-right">
+                            <a href="index.php" class="ac-home-sign-item-boutton-sign">
+                                Connexion
+                            </a>
+                        </li>
 
-            </div>
+                    </ul>
 
-        </ul>
+                </div>
 
-    </form>
+            </ul>
+
+        </form>
+
+    </div>
 
 </div>
