d56252f8e451a8f897ffb477f27f128ce2c3f8f8
TheoT <trolle.theo@gmail.com>
2018-03-26 01:01:23 +0200

    MAJ all day

--- view/pages/select_slot.php
+++ view/pages/select_slot.php
@@ -1,4 +1,4 @@
-<div class="ac-createEvent">
+<div class="ac-createEventMaj">
     <div class="ac-createEvent-header">
 
         <a href="index.php" id="close">
@@ -7,102 +7,122 @@
 
     </div>
 
-        <div class="ac-createEvent-body">
+    <div class="ac-createEven-body">
 
-            <div class="ac-createEvent-body-creneaux">
-                <form action="index.php" method="post">
-                    <input type="hidden" id="addEventTitle" name="nom" value="<?php echo$nom; ?>">
-                    <input type="hidden" placeholder="Description" name="description" value="<?php echo$description; ?>">
-                    <?php
-                    if(isset($users_choice_list)) {
-                        foreach ($users_choice_list as $user) {
-                            if(isset($user['right'])) {
-                                echo '<input type="hidden" name="users-choice[]" value="' . $user['id'] . '">';
-                            }
+        <div class="ac-createEvent-body-creneaux">
 
-                        }foreach ($users_choice_list as $user) {
-                            if(isset($user['right'])) {
-                                echo '<input type="hidden" name="user_right[]" value="' . $user['right'] . '">';
-                            }
-                        }
+            <form action="index.php" method="post">
 
-                        foreach(array_filter(array_unique(array_column($users_choice_list, "id_group"), null)) as $user) {
-                            if(isset($user)) {
-                                echo '<input type="hidden" name="groups-choice[]" value="' . $user . '">';
-                            }
+                <input type="hidden" id="addEventTitle" name="nom" value="<?php echo $nom; ?>">
+                <input type="hidden" placeholder="Description" name="description" value="<?php echo $description; ?>">
+
+                <?php
+                if (isset($users_choice_list)) {
+                    foreach ($users_choice_list as $user) {
+                        if (isset($user['right'])) {
+                            echo '<input type="hidden" name="users-choice[]" value="' . $user['id'] . '">';
                         }
 
                     }
-                    ?>
-                    <div class="ac-createEvent-body-creneaux-first">
+                    foreach ($users_choice_list as $user) {
+                        if (isset($user['right'])) {
+                            echo '<input type="hidden" name="user_right[]" value="' . $user['right'] . '">';
+                        }
+                    }
+
+                    foreach (array_filter(array_unique(array_column($users_choice_list, "id_group"), null)) as $user) {
+                        if (isset($user)) {
+                            echo '<input type="hidden" name="groups-choice[]" value="' . $user . '">';
+                        }
+                    }
+
+                }
+                ?>
+
+                <div class="ac-createEvent-body-creneaux-first">
 
                     <i class="ac-createEvent-body-creneaux-crenTxt">
                         &#xf017;
                     </i>
 
-                    <input class="ac-createEvent-body-creneaux-dateEv" type="Date" placeholder="type" name="start_date" required>
+                    <input class="ac-createEvent-body-creneaux-dateEv" type="Date" placeholder="type" name="start_date"
+                           required>
 
-                    <input class="ac-createEvent-body-creneaux-hoursEv" type="time" placeholder="type" name="start_time" required>
-
-                     </div>
-
-                     <div class="ac-createEvent-body-creneaux-second">
-
-                    <i class="ac-createEvent-body-creneaux-crenTxt">
-                        &#xf017;
-                    </i>
-
-                    <input class="ac-createEvent-body-creneaux-dateEv" type="Date" placeholder="type" name="end_date" required>
-
-                    <input class="ac-createEvent-body-creneaux-hoursEv" type="time" placeholder=type name="end_time" required>
+                    <input class="ac-createEvent-body-creneaux-hoursEv" type="time" placeholder="type" name="start_time"
+                           required>
 
                 </div>
-                <button type="submit" class="ac-createEvent-header-save" id="saveEvent"  name="action" value="create-event">
+
+                <div class="ac-createEvent-body-creneaux-second">
+
+                    <i class="ac-createEvent-body-creneaux-crenTxt">
+                        &#xf017;
+                    </i>
+
+                    <input class="ac-createEvent-body-creneaux-dateEv" type="Date" placeholder="type" name="end_date"
+                           required>
+
+                    <input class="ac-createEvent-body-creneaux-hoursEv" type="time" placeholder=type name="end_time"
+                           required>
+
+                </div>
+
+                <button type="submit" class="ac-createEvent-header-save" id="saveEvent" name="action"
+                        value="create-event">
                     enregistrer
                 </button>
-                </form>
-                <form action="index.php" method="post" >
-                    <input type="hidden" id="addEventTitle" name="nom" value="<?php echo$nom; ?>">
-                    <input type="hidden" placeholder="Description" name="description" value="<?php echo$description; ?>">
-                    <?php
 
-                    if(isset($users_choice_list)) {
-                        foreach ($users_choice_list as $user) {
-                            if(isset($user['right'])) {
-                                echo '<input type="hidden" name="users-choice[]" value="' . $user['id'] . '">';
-                            }
+            </form>
 
-                        }foreach ($users_choice_list as $user) {
-                            if(isset($user['right'])) {
-                                echo '<input type="hidden" name="user_right[]" value="' . $user['right'] . '">';
-                            }
-                        }
+            <form action="index.php" method="post">
+                <input type="hidden" id="addEventTitle" name="nom" value="<?php echo $nom; ?>">
+                <input type="hidden" placeholder="Description" name="description" value="<?php echo $description; ?>">
+                <?php
 
-                        foreach(array_filter(array_unique(array_column($users_choice_list, "id_group"), null)) as $user) {
-                            if(isset($user)) {
-                                echo '<input type="hidden" name="groups-choice[]" value="' . $user . '">';
-                            }
+                if (isset($users_choice_list)) {
+                    foreach ($users_choice_list as $user) {
+                        if (isset($user['right'])) {
+                            echo '<input type="hidden" name="users-choice[]" value="' . $user['id'] . '">';
                         }
 
                     }
-
-                    ?>
-                    <input type="hidden" id="addEventTitle" name="nom" value="<?php  echo$nom;  ?>">
-                    <input type="hidden" placeholder="Description" name="description" value="<?php echo$description; ?>">
-                    <div class="ac-createEvent-body-description">
-                        <?php
-                        foreach ($free_slot_list as $free_slot){
-                            echo'<input type ="radio" name="slot_list" value="'.$free_slot[1].','.$free_slot[2].','.$free_slot[3].','.$free_slot[4].'">'.$free_slot[1].','.$free_slot[2].','.$free_slot[3].','.$free_slot[4].' ';
+                    foreach ($users_choice_list as $user) {
+                        if (isset($user['right'])) {
+                            echo '<input type="hidden" name="user_right[]" value="' . $user['right'] . '">';
                         }
-                        ?>
-                        <button type="submit" class="ac-createEvent-header-save" id="saveEvent" name="action" value="create-event-by-slot-generator">
-                            valider
-                        </button>
-                    </div>
+                    }
 
-                </form>
-            </div>
+                    foreach (array_filter(array_unique(array_column($users_choice_list, "id_group"), null)) as $user) {
+                        if (isset($user)) {
+                            echo '<input type="hidden" name="groups-choice[]" value="' . $user . '">';
+                        }
+                    }
+
+                }
+
+                ?>
+
+                <input type="hidden" id="addEventTitle" name="nom" value="<?php echo $nom; ?>">
+                <input type="hidden" placeholder="Description" name="description" value="<?php echo $description; ?>">
+                <div class="ac-createEvent-body-description">
+
+                    <?php
+                    foreach ($free_slot_list as $free_slot) {
+                        echo '<input type ="radio" name="slot_list" value="' . $free_slot[1] . ',' . $free_slot[2] . ',' . $free_slot[3] . ',' . $free_slot[4] . '">' . $free_slot[1] . ',' . $free_slot[2] . ',' . $free_slot[3] . ',' . $free_slot[4] . ' ';
+                    }
+                    ?>
+
+                    <button type="submit" class="ac-createEvent-header-save" id="saveEvent" name="action"
+                            value="create-event-by-slot-generator">
+                        valider
+                    </button>
+
+                </div>
+
+            </form>
 
         </div>
+
+    </div>
 
 </div>
--- view/pages/select_slot_for_modification.php
+++ view/pages/select_slot_for_modification.php
@@ -1,77 +1,47 @@
-<div class="ac-createEvent">
+<div class="ac-createEventMaj">
     <div class="ac-createEvent-header">
 
-        <a href="index.php" class="close">
-            &#xf00d;
+        <a href="index.php" class="fa fa-close ac-createEvent-header-close">
         </a>
-
-
-
-
 
     </div>
 
-        <div class="ac-createEvent-body">
+    <div class="ac-createEvent-body">
 
-            <div class="ac-createEvent-body-creneaux">
-                <form action="index.php" method="post">
-                    <input type="hidden" id="addEventTitle" name="id_event" value="<?php  echo$id_event;  ?>">
-                    <div class="ac-createEvent-body-creneaux-first">
+        <div class="ac-createEvent-body-creneaux">
 
-                    <i class="ac-createEvent-body-creneaux-crenTxt">
-                        &#xf017;
-                    </i>
+            <form action="index.php" method="post">
 
-                    <input class="ac-createEvent-body-creneaux-dateEv" type="Date" placeholder="type" name="start_date" required>
+                <div class="ac-createEvent-body-description-radio">
 
-                    <input class="ac-createEvent-body-creneaux-hoursEv" type="time" placeholder="type" name="start_time" required>
+                    <?php
+                    foreach ($free_slot_list as $free_slot) {
 
-                     </div>
+                        echo '
+                        <div class="ac-createEvent-body-description-radio-item">
+                        
+                            <input class="ac-createEvent-body-radio" type ="radio" name="slot_list" value="' . $free_slot[1] . ',' . $free_slot[2] . ',' . $free_slot[3] . ',' . $free_slot[4] . '"> 
+                                <span class="ac-createEvent-body-radio-span">Le ' . $free_slot[1] . ' à ' . $free_slot[2] . ' au ' . $free_slot[3] . ' à ' . $free_slot[4] . '
+                                </span>
+                            </input>
+                            
+                        </div>';
+                    }
+                    ?>
 
-                     <div class="ac-createEvent-body-creneaux-second">
+                    <input type="hidden" id="addEventTitle" name="id_event" value="<?php echo $id_event; ?>">
 
-                    <i class="ac-createEvent-body-creneaux-crenTxt">
-                        &#xf017;
-                    </i>
-
-                    <input class="ac-createEvent-body-creneaux-dateEv" type="Date" placeholder="type" name="end_date" required>
-
-                    <input class="ac-createEvent-body-creneaux-hoursEv" type="time" placeholder=type name="end_time" required>
+                    <button type="submit" class="ac-createEvent-header-save" id="saveEvent" name="action"
+                            value="move-event-by-slot-generator">
+                        valider
+                    </button>
 
                 </div>
-                <button type="submit" class="ac-createEvent-header-save" id="saveEvent"  name="action" value="move-event">
-                    enregistrer
-                </button>
-                </form>
-                <form action="index.php" method="post" >
 
-
-
-                    <div class="ac-createEvent-body-description">
-                        <?php
-                        foreach ($free_slot_list as $free_slot){
-                            echo'<input type ="radio" name="slot_list" value="'.$free_slot[1].','.$free_slot[2].','.$free_slot[3].','.$free_slot[4].'">'.$free_slot[1].','.$free_slot[2].','.$free_slot[3].','.$free_slot[4].' ';
-                        }
-                        ?>
-                        <input type="hidden" id="addEventTitle" name="id_event" value="<?php  echo$id_event;  ?>">
-                        <button type="submit" class="ac-createEvent-header-save" id="saveEvent" name="action" value="move-event-by-slot-generator">
-                            valider
-                        </button>
-                    </div>
-
-                </form>
-            </div>
-
-
-
+            </form>
 
         </div>
 
-
-
-
-</div>
-
-</div>
+    </div>
 
 </div>
