5c8a17b4b4e4f09a68aab956eb9e224a1496fc4e
TheoT <trolle.theo@gmail.com>
2018-03-26 02:54:00 +0200

    selct slot maj

--- view/pages/select_slot.php
+++ view/pages/select_slot.php
@@ -1,13 +1,17 @@
 <div class="ac-createEventMaj">
     <div class="ac-createEvent-header">
 
-        <a href="index.php" id="close">
-            &#xf00d;
+        <a href="index.php" class="fa fa-close ac-createEvent-header-close">
+
         </a>
+
+        <h1 class="ac-createEventModif-header-title">
+            Liste des Créneaux disponible :
+        </h1>
 
     </div>
 
-    <div class="ac-createEven-body">
+    <div class="ac-createEvent-body">
 
         <div class="ac-createEvent-body-creneaux">
 
@@ -67,7 +71,7 @@
 
                 </div>
 
-                <button type="submit" class="ac-createEvent-header-save" id="saveEvent" name="action"
+                <button type="submit" class="ac-createEvent-header-save ac-createEventModif-header-save" id="saveEvent" name="action"
                         value="create-event">
                     enregistrer
                 </button>
@@ -75,8 +79,10 @@
             </form>
 
             <form action="index.php" method="post">
+
                 <input type="hidden" id="addEventTitle" name="nom" value="<?php echo $nom; ?>">
                 <input type="hidden" placeholder="Description" name="description" value="<?php echo $description; ?>">
+
                 <?php
 
                 if (isset($users_choice_list)) {
@@ -108,11 +114,20 @@
 
                     <?php
                     foreach ($free_slot_list as $free_slot) {
-                        echo '<input type ="radio" name="slot_list" value="' . $free_slot[1] . ',' . $free_slot[2] . ',' . $free_slot[3] . ',' . $free_slot[4] . '">' . $free_slot[1] . ',' . $free_slot[2] . ',' . $free_slot[3] . ',' . $free_slot[4] . ' ';
+
+                        echo '
+                        <div class="ac-createEvent-body-description-radio-item">
+                        
+                            <input class="ac-createEvent-body-radio" type ="radio" name="slot_list" value="' . $free_slot[1] . ',' . $free_slot[2] . ',' . $free_slot[3] . ',' . $free_slot[4] . '"> 
+                                <span class="ac-createEvent-body-radio-span">Le ' . $free_slot[1] . ' à ' . $free_slot[2] . ' au ' . $free_slot[3] . ' à ' . $free_slot[4] . '
+                                </span>
+                            </input>
+                            
+                        </div>';
                     }
                     ?>
 
-                    <button type="submit" class="ac-createEvent-header-save" id="saveEvent" name="action"
+                    <button type="submit" class="ac-createEvent-header-save ac-createEventModif-header-save" id="saveEvent" name="action"
                             value="create-event-by-slot-generator">
                         valider
                     </button>
